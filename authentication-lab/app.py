af99a8a9fd7d47b44b29010e9c54eb9540e909e6
zena24-meet <siatbr.siatbr@gmail.com>
2023-07-25 09:56:01 +0300

    zena

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -9,13 +9,14 @@ config = {
   "storageBucket": "fir-17f02.appspot.com",
   "messagingSenderId": "787139879406",
   "appId": "1:787139879406:web:e62a546ade52375942eeff",
-  "measurementId": "G-K9LW85TTZH", "databaseURL":""}
+  "measurementId": "G-K9LW85TTZH", "databaseURL":"https://fir-17f02-default-rtdb.firebaseio.com/"}
 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db= firebase.database()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -25,7 +26,7 @@ def signin():
         password = request.form['password']
         try:
             login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-            return redirect(url_for('add_tweet.html'))
+            return redirect(url_for('add_tweet'))
         except:
             error = "Authentication failed"
 
@@ -38,10 +39,15 @@ def signup():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
+        fullname = request.form['fullname']
+        username = request.form['username']
+        bio = request.form['bio']
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
-
-            return redirect(url_for('add_tweet.html'))
+            UID = login_session['user']['localId']
+            user = {"fullname":fullname,"username": username,"email":email,"password":password,"bio":bio}
+            db.child("Users").child(UID).set(user)
+            return redirect(url_for('add_tweet'))
         except:
             error = "Authentication failed"
 
@@ -50,8 +56,22 @@ def signup():
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method == "POST":
+        title = request.form['title']
+        tweet = request.form['tweet']
+        try:
+            UID = login_session['tweet']['localId']
+            tweet = {"title": title,"tweet": tweet,"UID":UID}
+            db.child("tweet").push(tweet)
+        except:
+            error = "Authentication failed"
+            return redirect(url_for('add_tweet'))
     return render_template("add_tweet.html")
 
+@app.route('/all_tweets',methods=["GET","POST"])
+def all_tweets():
+    tweets=db.child("tweet").get().val()
+    return render_template("tweets.html", tweets=tweets)
 
 if __name__ == '__main__':
     app.run(debug=True)
